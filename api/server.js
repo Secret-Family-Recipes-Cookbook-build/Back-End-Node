1aadf94b57108e9672572909a810ed32f70d5de2
jonathan majors <izzymajors@gmail.com>
2021-03-30 12:41:55 -0500

    heroku push

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express= require("express");
 const server = express();
 const cors = require("cors")
+const helmet = require("helmet")
 
 server.use(express.json());
 server.use(cors());
+server.use(helmet());
 
 server.get("/", (req, res) => {
     res.status(200).json({ api: "up" });
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 
+const path = require('path');
+const express = require('express')
+
 const server = require('./api/server.js');
 
 const port = process.env.PORT || 5000;
+
+server.use(express.static(path.join(__dirname, 'client/dist')))
+
 server.listen(port, () => console.log(`\n** server up on port ${port} **\n`));
 
 
